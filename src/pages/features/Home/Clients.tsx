48a17bda7fd4e36ab88998a536bbc6132ee60dc6
Caio Brotzki <caio.brotzki@gmail.com>
2025-08-29 15:59:13 -0300

    abuhh

--- src/pages/features/Home/Clients.tsx
+++ src/pages/features/Home/Clients.tsx
@@ -3,10 +3,10 @@ import Logo2 from "../../../assets/Logo2.svg";
 import Logo3 from "../../../assets/Logo3.svg";
 import Logo4 from "../../../assets/Logo4.svg";
 import Logo5 from "../../../assets/Logo5.svg";
-import Logo6 from "../../../assets/Logo6.svg";
+
 import Logo7 from "../../../assets/Logo7.svg";
 
-const logos = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7];
+const logos = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo7];
 
 function Client() {
   return (
